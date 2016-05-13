105d3b467eae60bc2988c6c94156e5487f8da8ec
nik-nik-1 <dementievnikolay@gmail.com>
2016-05-13 11:09:44 +0300

    first commit

---
+++ CircleTransition/CircleViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CircleViewController.swift
+//  CircleTransition
+//
+//  Created by mc373 on 13.05.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+import UIKit
+
+class CircleViewController: UIViewController {
+
+}
