2d279e93747dc0454b4a3853c4d8c89644a5e6e0
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-07-08 16:54:05 +0300

    image change

--- CircleTransition/NavigationControllerDelegate.swift
+++ CircleTransition/NavigationControllerDelegate.swift
@@ -10,12 +10,60 @@ import UIKit
 
 class NavigationControllerDelegate: NSObject, UINavigationControllerDelegate {
     
-//    This method receives the two view controllers that the navigation controller is transitioning
-//    between, and its job is to return an object that implements
-//    UIViewControllerAnimatedTransitioning.
+    //    This method receives the two view controllers that the navigation controller is transitioning
+    //    between, and its job is to return an object that implements
+    //    UIViewControllerAnimatedTransitioning.
     func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         
         return  CircleTransitionAnimator()
     }
-
+    
+    //MARK: An Interactive Gesture Animation
+    @IBOutlet weak var navigationController: UINavigationController?
+    
+    var interactionController: UIPercentDrivenInteractiveTransition?
+    
+    func navigationController(navigationController: UINavigationController,
+                              interactionControllerForAnimationController animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return self.interactionController
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(NavigationControllerDelegate.panned(_:)))
+        self.navigationController!.view.addGestureRecognizer(panGesture)
+    }
+    
+    //1
+    @IBAction func panned(gestureRecognizer: UIPanGestureRecognizer) {
+        switch gestureRecognizer.state {
+        case .Began:
+            self.interactionController = UIPercentDrivenInteractiveTransition()
+            if self.navigationController?.viewControllers.count > 1 {
+                self.navigationController?.popViewControllerAnimated(true)
+            } else {
+                self.navigationController?.topViewController!.performSegueWithIdentifier("PushSegue", sender: nil)
+            }
+            
+        //2
+        case .Changed:
+            let translation = gestureRecognizer.translationInView(self.navigationController!.view)
+            let completionProgress = translation.x/CGRectGetWidth(self.navigationController!.view.bounds)
+            self.interactionController?.updateInteractiveTransition(completionProgress)
+            
+        //3
+        case .Ended:
+            if (gestureRecognizer.velocityInView(self.navigationController!.view).x > 0) {
+                self.interactionController?.finishInteractiveTransition()
+            } else {
+                self.interactionController?.cancelInteractiveTransition()
+            }
+            self.interactionController = nil
+            
+        //4
+        default:
+            self.interactionController?.cancelInteractiveTransition()
+            self.interactionController = nil
+        }
+    }
 }
